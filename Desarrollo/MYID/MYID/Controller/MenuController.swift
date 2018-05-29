7efbe2efc8ac621bb97c96c000dca37f7a1e007e
MacBookDesarrolloTecno01 <macbookdesarrollotecno01@MacTecno2.local>
2018-05-29 14:06:47 -0600

    colores

---
+++ Desarrollo/MYID/MYID/Controller/MenuController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MenuController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
